061b76611c78ae4b8839ab560765d18297f41cb1
SPbCT-AnnaLobanova <annaloft251@gmail.com>
2021-03-15 21:40:45 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let strKey = str.length; strKey >= 0; strKey--) {
+        for (let bracketsKey = 0; bracketsKey < bracketsConfig.length; bracketsKey++) {
+            if (str[strKey] === bracketsConfig[bracketsKey][0]) {
+                str = str.replace(bracketsConfig[bracketsKey].join(""), "");
+            }
+        }
+    }
+
+    return str === "";
 }
+
+// your solution
